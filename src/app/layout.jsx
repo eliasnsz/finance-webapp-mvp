b3354f74f3894b6d10f5abff1e3e6f4da70f52db
Elias Souza <eliasnsz.dev@gmail.com>
2026-07-22 14:40:59 -0300

    style: fix linting errors

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="pt-BR">
+			<body>{children}</body>
+		</html>
+	);
 }
