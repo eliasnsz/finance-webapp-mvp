2e1077d0f2ad3118562ed76f40c3d631b8ff53d8
Elias Souza <eliasnsz.dev@gmail.com>
2026-07-22 16:29:00 -0300

    feat: setup local `postgres` database

--- tests/api/v1/status/get.spec.js
+++ tests/api/v1/status/get.spec.js
@@ -6,10 +6,19 @@ describe("GET /api/v1/status", () => {
 		await orchestrator.waitForWebServer();
 	});
 
-	it("should return 'healthy' status", async () => {
+	it("should return 'healthy' status and database info", async () => {
 		const response = await fetch("http://localhost:3000/api/v1/status");
 		const responseBody = await response.json();
 
 		expect(responseBody.status).toBe("healthy");
+		expect(responseBody.database).toEqual(
+			expect.objectContaining({
+				max_connections: expect.any(Number),
+				total_used_connections: expect.any(Number),
+				active_connections: expect.any(Number),
+				idle_connections: expect.any(Number),
+				remaining_slots: expect.any(Number),
+			}),
+		);
 	});
 });
